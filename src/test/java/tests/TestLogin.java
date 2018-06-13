d00c2d9da1b102375febfe1029d281391b7c7127
Kazeem <kazeem.omisore@saucelabs.com>
2018-06-13 23:42:24 +0100

    Initial commit

---
+++ src/test/java/tests/TestLogin.java
@@ -0,0 +1,45 @@
+//filename: tests/TestLogin.java
+package tests;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.Assert.*;
+
+public class TestLogin
+{
+    private WebDriver driver;
+
+    @Before
+    public void setUp()
+    {
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/vendor/geckodriver");
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void succeeded()
+    {
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button")).click();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue("Success message not present",
+                driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+}
